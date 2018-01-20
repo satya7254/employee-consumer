3bb477972bd398f51d5979123746f5c75f7200ce
Satya <Satya@Satya-PC>
2018-01-20 21:20:39 +0530

    sysout changed

--- src/main/java/com/satya/controllers/EmpConsumerControllerClient.java
+++ src/main/java/com/satya/controllers/EmpConsumerControllerClient.java
@@ -22,6 +22,7 @@ public class EmpConsumerControllerClient {
 			System.out.println(ex);
 		}
 		System.out.println(response.getBody());
+		System.out.println("End of main");
 	}
 
 	private static HttpEntity<?> getHeaders() throws IOException {
